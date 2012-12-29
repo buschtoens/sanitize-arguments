074a9795ba980be7cf655dc05dfdf9c529961a77
Jan Buschtöns <buschtoens@gmail.com>
2012-12-30 00:01:42 +0100

    One code style to rule them all

--- test/sanitize.js
+++ test/sanitize.js
@@ -1,10 +1,10 @@
 // Require the lib
 var sanitize = require("../")
-	, assert = require('assert');
+	, assert = require("assert");
 
-describe('Sanitize', function() {
-	describe('Without Function Reference', function() {
-		it('should return an array', function() {
+describe("Sanitize", function() {
+	describe("Without Function Reference", function() {
+		it("should return an array", function() {
 			var foo = function(a, b, c) {
 						var params = sanitize(arguments, [String, String, String]);
 						
@@ -14,7 +14,7 @@ describe('Sanitize', function() {
 			foo();
 		});
 		
-		it('should return an array with the same amount of arguments as it was called with', function() {
+		it("should return an array with the same amount of arguments as it was called with", function() {
 			var foo = function(num, b, c) {
 						var params = sanitize(arguments, [Number, String, String]);
 						
@@ -22,25 +22,25 @@ describe('Sanitize', function() {
 					};
 			
 			foo(1);
-			foo(2, 'b');
-			foo(3, 'b', 'c');
+			foo(2, "b");
+			foo(3, "b", "c");
 		});
 		
-		it('should handle arguments that are out of order', function() {
+		it("should handle arguments that are out of order", function() {
 			var foo = function(a, b, c, r) {
 						var params = sanitize(arguments, [String, Function, Number]);
 						
-						assert(typeof params[0] == 'string');
-						assert(typeof params[1] == 'function');
-						assert(typeof params[2] == 'number');
+						assert(typeof params[0] == "string");
+						assert(typeof params[1] == "function");
+						assert(typeof params[2] == "number");
 					};
 			
-			foo('a', function() {}, 1);
-			foo(1, 'a', function() {});
-			foo(function() {}, 1, 'a');
+			foo("a", function() {}, 1);
+			foo(1, "a", function() {});
+			foo(function() {}, 1, "a");
 		});
 		
-		it('should handle default values', function() {
+		it("should handle default values", function() {
 			var foo = function(a, b, c, d) {
 						var params = sanitize(arguments, [String, [Function, function defaultFunction() {} ], [1], [String, false] ]);
 						
@@ -48,67 +48,67 @@ describe('Sanitize', function() {
 					}
 				, params;
 			   
-			params = foo('test');
+			params = foo("test");
 			
-			assert(params[0] === 'test');
-			assert(typeof params[1] == 'function' && params[1].name == 'defaultFunction');
+			assert(params[0] === "test");
+			assert(typeof params[1] == "function" && params[1].name == "defaultFunction");
 			assert(params[2] === 1);
 			assert(params[3] === false);
 			
-			params = foo('again', function notDefaultFunction() {}, 333, 'testing');
+			params = foo("again", function notDefaultFunction() {}, 333, "testing");
 			
-			assert(params[0] === 'again');
-			assert(typeof params[1] == 'function' && params[1].name == 'notDefaultFunction');
+			assert(params[0] === "again");
+			assert(typeof params[1] == "function" && params[1].name == "notDefaultFunction");
 			assert(params[2] === 333);
-			assert(params[3] === 'testing');
+			assert(params[3] === "testing");
 			
-			params = foo('again', 'testing', function notDefaultFunction() {}, 333);
+			params = foo("again", "testing", function notDefaultFunction() {}, 333);
 			
-			assert(params[0] === 'again');
-			assert(typeof params[1] == 'function' && params[1].name == 'notDefaultFunction');
+			assert(params[0] === "again");
+			assert(typeof params[1] == "function" && params[1].name == "notDefaultFunction");
 			assert(params[2] === 333);
-			assert(params[3] === 'testing');
+			assert(params[3] === "testing");
 		});
 	});
 	
-	describe('With Function Reference', function() {
-		it('should return an object', function() {
+	describe("With Function Reference", function() {
+		it("should return an object", function() {
 			var foo = function(a, b, c) {
 						var params = sanitize(arguments, foo, [String, String, String]);
 						
-						assert(typeof params == 'object');
+						assert(typeof params == "object");
 					};
 			
 			foo();
 		});
 		
-		it('should return an array with the same amount of arguments as it was called with', function() {
+		it("should return an array with the same amount of arguments as it was called with", function() {
 			var foo = function(a, b, c) {
 						var params = sanitize(arguments, foo, [String, String, String]);
 						
-						assert(params.a == 'a');	
-						assert(params.b == 'b');
-						assert(params.c == 'c');
+						assert(params.a == "a");	
+						assert(params.b == "b");
+						assert(params.c == "c");
 					};
 			
-			foo('a', 'b', 'c');
+			foo("a", "b", "c");
 		});
 		
-		it('should handle arguments that are out of order', function() {
+		it("should handle arguments that are out of order", function() {
 			var foo = function(a, b, c) {
 						var params = sanitize(arguments, foo, [String, Function, Number]);
 						
-						assert(typeof params.a == 'string');
-						assert(typeof params.b == 'function');
-						assert(typeof params.c == 'number');
+						assert(typeof params.a == "string");
+						assert(typeof params.b == "function");
+						assert(typeof params.c == "number");
 					};
 			
-			foo('a', function() {}, 1);
-			foo(1, 'a', function() {});
-			foo(function() {}, 1, 'a');
+			foo("a", function() {}, 1);
+			foo(1, "a", function() {});
+			foo(function() {}, 1, "a");
 		});
 		
-		it('should handle default values', function() {
+		it("should handle default values", function() {
 			var foo = function(a, b, c, d) {
 						var params = sanitize(arguments, foo, [String, [Function, function defaultFunction() {} ], [1], [String, false]]);
 						
@@ -116,26 +116,26 @@ describe('Sanitize', function() {
 					}
 				, params;
 			   
-			params = foo('test');
+			params = foo("test");
 			
-			assert(params.a === 'test');
-			assert(typeof params.b == 'function' && params.b.name == 'defaultFunction');
+			assert(params.a === "test");
+			assert(typeof params.b == "function" && params.b.name == "defaultFunction");
 			assert(params.c === 1);
 			assert(params.d === false);
 			
-			params = foo('again', function notDefaultFunction() {}, 333, 'testing');
+			params = foo("again", function notDefaultFunction() {}, 333, "testing");
 			
-			assert(params.a === 'again');
-			assert(typeof params.b == 'function' && params.b.name == 'notDefaultFunction');
+			assert(params.a === "again");
+			assert(typeof params.b == "function" && params.b.name == "notDefaultFunction");
 			assert(params.c === 333);
-			assert(params.d === 'testing');
+			assert(params.d === "testing");
 			
-			params = foo('again', 'testing', function notDefaultFunction() {}, 333);
+			params = foo("again", "testing", function notDefaultFunction() {}, 333);
 			
-			assert(params.a === 'again');
-			assert(typeof params.b == 'function' && params.b.name == 'notDefaultFunction');
+			assert(params.a === "again");
+			assert(typeof params.b == "function" && params.b.name == "notDefaultFunction");
 			assert(params.c === 333);
-			assert(params.d === 'testing');
+			assert(params.d === "testing");
 		});
 	});
 });
--- test/typechecks.js
+++ test/typechecks.js
@@ -52,16 +52,13 @@ describe("typeOf(obj)", function() {
 		it("should return 'Object' when obj is an Object", function() {
 			typeOf({}).should.eql("Object");
 		});
-                
 	});
-        
-        describe("Custom Objects", function() {
+	
+	describe("Custom Objects", function() {
 		it("should return 'ClassName' when obj is an instance of function ClassName", function() {
-                    function ClassName() {};
-                    
-                    var foo = new ClassName();                                        
-                    
-		    typeOf(foo).should.eql("ClassName");
+			function ClassName() {};
+			var foo = new ClassName();
+			typeOf(foo).should.eql("ClassName");
 		});
 	});
 	
